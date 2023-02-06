6f25d35b5dc1d7dab170daad043f6fdaa7e7ce03
Murat Istek <messi897@gmail.com>
2023-02-06 18:50:24 -0500

    Create frontend UI and move it to "public" folder

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const http = require('http')
+const path = require('path')
 const mongoose = require('mongoose')
 const userRouter = require('./routes/UserRoutes.js')
+const socketio = require('socket.io')
 
 const app = express()
-// app.use(express.json())
+const server = http.createServer(app)
+const io = socketio(server)
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Execute if client connects 
+io.on('connection', socket => {
+  console.log('new socket connection for the user');
+});
 
 // Connection to mongoDB database
 mongoose.connect('mongodb+srv://murat96:123456murka@cluster0.okt8nyq.mongodb.net/w2023_comp3133?retryWrites=true&w=majority', {
@@ -17,5 +29,14 @@ mongoose.connect('mongodb+srv://murat96:123456murka@cluster0.okt8nyq.mongodb.net
 
 app.use(userRouter)
 
+app.get('/', (req, res) => {
+  res.redirect('/signup')
+})
 
-app.listen(3005, () => console.log("The server is running on port 3005"))
+// Frontend redirect
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+server.listen(3005, () => console.log("The server is running on port 3005"))
