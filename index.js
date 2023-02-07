6057b45c5e0d245d555b4fe1acc62b33d8a65dac
Murat Istek <messi897@gmail.com>
2023-02-06 20:12:24 -0500

    Add socket.io functionality and connected frontend

--- index.js
+++ index.js
@@ -9,12 +9,46 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+function getFormattedTime() {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let am_pm = 'AM';
+  if (hours >= 12) {
+    am_pm = 'PM';
+    hours = hours % 12 || 12;
+  }
+
+  return `  ${hours}:${minutes}:${seconds} ${am_pm}`;
+}
+
+function returnUpdatedMessage(username, text) {
+  return {
+    username,
+    text,
+    createdAt: getFormattedTime()
+  }
+}
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Execute if client connects 
 io.on('connection', socket => {
-  console.log('new socket connection for the user');
+  socket.emit('message', returnUpdatedMessage('Murat the creator', 'Welcome to my chat app'))
+  socket.broadcast.emit('message', returnUpdatedMessage('Murat the creator', 'User joined the chat'))
+
+  // Execute if client disconnected
+  socket.on('disconnect', () => {
+    io.emit('message', returnUpdatedMessage('Murat the creator', 'User has left'))
+  })
+
+  // Check userChatMessage from server
+  socket.on('userChatMessage', (msg) => {
+    io.emit('message', returnUpdatedMessage('USER', msg));
+  })
 });
 
 // Connection to mongoDB database
@@ -28,15 +62,6 @@ mongoose.connect('mongodb+srv://murat96:123456murka@cluster0.okt8nyq.mongodb.net
 });
 
 app.use(userRouter)
-
-app.get('/', (req, res) => {
-  res.redirect('/signup')
-})
-
-// Frontend redirect
-app.get('/signup', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
 
 
 server.listen(3005, () => console.log("The server is running on port 3005"))
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,29 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
+
+// Submit message
+socket.on('message', message => {
+  console.log(message);
+  addMessageToDOM(message);
+})
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
+  // Emit message to server
+  socket.emit('userChatMessage', msg);
+  e.target.elements.msg.value = '';
+})
+
+// Add sent message to DOM
+function addMessageToDOM(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `
+  <p class="msgInfo">${message.username}<span>${message.createdAt}</span></p>
+  <p class="chatText">
+    ${message.text}
+  </p>`;
+  document.querySelector('.chat-msgs').appendChild(div);
+}
